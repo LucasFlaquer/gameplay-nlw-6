c939571155396bc7c77557f8e6a65569647719c7
LucasFlaquer <lucas.flaquer@gmail.com>
2021-06-26 16:12:19 -0300

    setup prettier eslint

--- App.tsx
+++ App.tsx
@@ -1,34 +1,44 @@
-import { AuthContext, AuthProvider } from './src/hooks/auth';
-import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
-import { LogBox, StatusBar } from 'react-native';
-import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani'
+import React from 'react'
+import { AuthProvider } from './src/hooks/auth'
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import { LogBox, StatusBar } from 'react-native'
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani'
 
-import AppLoading from 'expo-app-loading';
-import { Background } from './src/components/Background';
-import React from 'react';
-import { Routes } from './src/routes';
-import {useFonts} from 'expo-font'
+import AppLoading from 'expo-app-loading'
+import { Background } from './src/components/Background'
 
-LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine'])
+import { useFonts } from 'expo-font'
+import { Routes } from './src/routes'
+
+LogBox.ignoreLogs([
+  'You are not currently signed in to Expo on your development machine',
+])
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
     Rajdhani_500Medium,
-    Rajdhani_700Bold
-  });
-  
-  if(!fontsLoaded) {
+    Rajdhani_700Bold,
+  })
+
+  if (!fontsLoaded) {
     return <AppLoading />
   }
 
   return (
     <Background>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       <AuthProvider>
         <Routes />
       </AuthProvider>
     </Background>
-  );
+  )
 }

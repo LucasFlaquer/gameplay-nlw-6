41c02af9c88a0574ab85fb5e912c5f3f15810fdb
LucasFlaquer <lucas.flaquer@gmail.com>
2021-06-26 16:43:44 -0300

    lint home and organize imports

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,18 @@
-import { Appointment, AppointmentProps } from '../../components/Appointment';
-import {FlatList, Text, View} from 'react-native'
-import React, { useCallback, useState } from 'react';
-import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import React, { useCallback, useState } from 'react'
+import { FlatList, View } from 'react-native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Background } from '../../components/Background';
-import { ButtonAdd } from '../../components/ButtonAdd';
-import { COLLECTION_APPOINTMENTS } from '../../configs/database';
-import { CategorySelect } from '../../components/CategorySelect';
-import { ListDivider } from '../../components/ListDivider';
-import { ListHeader } from '../../components/ListHeader';
-import { Loading } from '../../components/Loading';
-import { Profile } from '../../components/Profile';
-import { styles } from './styles';
+import { Appointment, AppointmentProps } from '../../components/Appointment'
+import { Background } from '../../components/Background'
+import { ButtonAdd } from '../../components/ButtonAdd'
+import { COLLECTION_APPOINTMENTS } from '../../configs/database'
+import { CategorySelect } from '../../components/CategorySelect'
+import { ListDivider } from '../../components/ListDivider'
+import { ListHeader } from '../../components/ListHeader'
+import { Loading } from '../../components/Loading'
+import { Profile } from '../../components/Profile'
+import { styles } from './styles'
 
 export function Home() {
   const [category, setCategory] = useState('')
@@ -25,27 +25,28 @@ export function Home() {
   }
 
   function handleApointmentDetails(guildSelected: AppointmentProps) {
-
     navigation.navigate('AppointmentDetails', { guildSelected })
   }
   function handleApointmentCreate() {
-    navigation.navigate("AppointmentCreate")
+    navigation.navigate('AppointmentCreate')
   }
 
   async function loadAppointments() {
     const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS)
     const storage: AppointmentProps[] = response ? JSON.parse(response) : []
-    if(category) {
-      setAppointments(storage.filter(item=>item.category === category))
+    if (category) {
+      setAppointments(storage.filter((item) => item.category === category))
     } else {
       setAppointments(storage)
     }
     setLoading(false)
   }
-   
-  useFocusEffect(useCallback(()=> {
-    loadAppointments()
-  }, [category]))
+
+  useFocusEffect(
+    useCallback(() => {
+      loadAppointments()
+    }, [category]),
+  )
 
   return (
     <Background>
@@ -53,30 +54,34 @@ export function Home() {
         <Profile />
         <ButtonAdd onPress={handleApointmentCreate} />
       </View>
-      <CategorySelect 
+      <CategorySelect
         categorySelected={category}
         setCategory={handleCategorySelect}
       />
-      {
-        loading ? <Loading /> :
+      {loading ? (
+        <Loading />
+      ) : (
         <>
-          <ListHeader title="Partidas Agendadas" subtitle={`Total ${appointments.length}`}/>
-          <FlatList  
+          <ListHeader
+            title='Partidas Agendadas'
+            subtitle={`Total ${appointments.length}`}
+          />
+          <FlatList
             data={appointments}
-            keyExtractor={item => item.id}
+            keyExtractor={(item) => item.id}
             style={styles.matches}
             showsHorizontalScrollIndicator={false}
             ItemSeparatorComponent={ListDivider}
-            contentContainerStyle={{paddingBottom: 69}}
-            renderItem={({item}) => (
-              <Appointment 
-                data={item} 
-                onPress={()=> handleApointmentDetails(item)}
+            contentContainerStyle={{ paddingBottom: 69 }}
+            renderItem={({ item }) => (
+              <Appointment
+                data={item}
+                onPress={() => handleApointmentDetails(item)}
               />
             )}
           />
         </>
-      }
+      )}
     </Background>
   )
 }
